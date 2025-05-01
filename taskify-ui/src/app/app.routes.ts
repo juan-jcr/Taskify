2d00cdb844a41b7e77c65aef60719448e418568a
Juan Antonio Castañeda <castaneda2850@gmail.com>
2025-04-30 22:23:12 -0500

    page login

--- taskify-ui/src/app/app.routes.ts
+++ taskify-ui/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import LoginComponent from './auth/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/layout/auth.layout.component'),
+    children: [
+      {path: 'login', component: LoginComponent}
+    ]
+  },
+];
---
+++ taskify-ui/src/app/auth/layout/auth.layout.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-auth.layout',
+  imports: [RouterOutlet],
+  template: `<div
+    class="min-h-screen flex  justify-center
+   bg-gradient-to-r from-gray-100 via-gray-300"
+  >
+    <div>
+      <h1 class="text-4xl text-gray-700 my-14 tracking-[6px] font-semibold text-center">
+        <span class="text-blue-500">Task</span>ify
+      </h1>
+      <router-outlet />
+    </div>
+  </div>`,
+})
+export default class AuthLayoutComponent {}
